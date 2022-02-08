8383e601b6898b39cc61473af952682324696ed9
Antoine Lair <62932641+LAIR-Antoine@users.noreply.github.com>
2022-02-08 12:14:17 +0100

    Init

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Antoine Lair</title>
+		<link rel="stylesheet" href="style.css">
+		<!-- Import font Google -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+		<!-- Import icons Font Awesome -->
+		<script src="https://kit.fontawesome.com/58dd6bd8ca.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li><a href="#home">Accueil</a></li>
+					<li><a href="#about">A propos</a></li>
+					<li><a href="#projects">Réalisations</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section id="home">
+			<h1>Bienvenue !</h1>
+		</section>
+		<section id="about"></section>
+		<section id="projects">
+<!-- 			<a href="">
+				<img src="" alt="">
+			</a> -->
+		</section>
+		<section id="contact"></section>
+		<footer>
+			<p class="copyright">&copy; Antoine Lair - 2022</p>
+			<p>
+				<a href="https://www.linkedin.com/in/lairantoine89/" target="_blank"><i class="fab fa-linkedin"></i></a>
+				<a href="https://github.com/LAIR-Antoine/" target="_blank"><i class="fab fa-github"></i></a>
+			</p>
+			
+		</footer>
+	</body>
+</html>
