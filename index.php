7e0704e6efbf9966ad04e2f6c32e40d31204432f
Antoine Lair <62932641+LAIR-Antoine@users.noreply.github.com>
2022-05-04 16:38:47 +0200

    Big update

--- index.php
+++ index.php
@@ -11,34 +11,151 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<!-- Import icons Font Awesome -->
 	<script src="https://kit.fontawesome.com/58dd6bd8ca.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
 	<header>
-		<nav>
-			<ul>
-				<li><a href="#home">Accueil</a></li>
-				<li><a href="#about">à propos</a></li>
-				<li><a href="#projects">Réalisations</a></li>
-				<li><a href="#contact">Contact</a></li>
-			</ul>
-		</nav>
+		<div class="nav-container">
+
+			<nav>
+				<div class="nav-mobile"><a id="navbar-toggle" alt="nav-button" aria-label="nav-button" href="#!"><span></span></a></div>
+				<ul class="nav-list">
+					<li>
+						<a href="#home">Accueil</a>
+					</li>
+					<li>
+						<a href="#about">à propos</a>
+					</li>
+					<li>
+						<a href="#projects">Réalisations</a>
+					</li>
+
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+
 	</header>
+
+	<script>
+		(function($) {
+			$(function() {
+
+				//  open and close nav 
+				$('#navbar-toggle').click(function() {
+					$('nav ul').slideToggle();
+				});
+
+
+				// Hamburger toggle
+				$('#navbar-toggle').on('click', function() {
+					this.classList.toggle('active');
+				});
+
+
+				// If a link has a dropdown, add sub menu toggle.
+				$('nav ul li a:not(:only-child)').click(function(e) {
+					$(this).siblings('.navbar-dropdown').slideToggle("slow");
+
+					// Close dropdown when select another dropdown
+					$('.navbar-dropdown').not($(this).siblings()).hide("slow");
+					e.stopPropagation();
+				});
+
+
+				// Click outside the dropdown will remove the dropdown class
+				$('html').click(function() {
+					$('.navbar-dropdown').hide();
+				});
+			});
+		})(jQuery);
+	</script>
+
 	<section id="home">
-		<h1>Bienvenu !</h1>
+		<h1>Bienvenue !</h1>
 		<p>Je suis Antoine Lair, </p>
 		<p class="typing">Développeur Full-stack</p>
 	</section>
 	<section id="about">
-		<p>Actuellement étudiant...</p>
-	</section>
-	<section id="projects">
+		<div class="aboutleft">
+			<img src="./images/profil.jpg" alt="Ma photo de profil">
+			<a class="button" href="./files/cvVF.pdf" target="_blank">Lire mon CV</a>
+		</div>
+		<div class="aboutright">
+		<p>Je suis <span>Antoine Lair</span>, étudiant en <span>BUT MMI</span> à l’IUT de Troyes. Je suis cette formation avec l’objectif de devenir <span>développeur full-stack</span>.</p>
+			<p>J’ai été étudiant à l’<span>UTT</span> pendant <span>deux ans</span> avant de me réorienter vers MMI qui me correspond mieux. J’y ai découvert le <span>développement web</span> à l’UNG, une association étudiante.</p>
+			<p>Ces années de dev m’ont permis de réaliser <span>plusieurs projets</span> comme le site de mon <span>club de triathlon</span> à Sens, qui est actuellement le plus abouti.</p>
+			<p>Je suis actuellement à la recherche d’une <span>alternance pour 2 ans</span> à partir de <span>septembre 2022</span> dans le <span>développement web</span> pour continuer à m’améliorer.</p>
+			
+		</div>
+		
 
 	</section>
-	<section id="contact"></section>
+	<section id="projects">
+		<div>
+			<a href="https://sens-triathlon.com" target="_blank">
+				<img src="./images/senstri.png" alt="">
+			</a>
+			<p>Depuis septembre 2019, j'ai développé et je maintiens à jour le site de Sens Triathlon.</p>
+			<p>Le site était initialement sur un Wordpress mis en place en 2014. J'ai donc proposé de le modernisé et il a été réalisé en React (framework JS).</p>
+			<p class="center"><i class="fab fa-react"></i> React</p>
+			<p class="center"><i class="fab fa-node-js"></i> Node.js</p>
+		</div>
+		<div>
+			<a href="https://mmi21b10.sae203.ovh/" target="_blank">
+				<img src="./images/iut.png" alt="">
+			</a>
+			<p>Site réalisé sur 2 semaines en mars 2022 dans le cadre de ma formation à l'IUT de Troyes (BUT MMI).</p>
+			<p>Ce projet avait pour but de nous faire découvrir la mise en place d'un site utilisant une base de donnée. </p>
+			<p class="center"><i class="fab fa-php"></i> PHP</p>
+			<p class="center"><i class="fas fa-database"></i> MariaDB / PhpMyAdmin</p>
+		</div>
+		<div>
+			<a href="https://eve.lairantoine.fr/" target="_blank">
+				<img src="./images/eve.png" alt="">
+			</a>
+			<p>Réalisé de septembre 2019 à février 2020, ce projet est ma première réalisation sur une longue durée.</p>
+			<p>Il était relié à une API que j'avais développé en Node.js.</p>
+			<p class="center"><i class="fab fa-react"></i> React</p>
+			<p class="center"><i class="fab fa-node-js"></i> Node.js</p>
+			<p class="center"><i class="fas fa-database"></i> MariaDB / Adminer</p>
+		</div>
+	</section>
+	<section id="contact">
+		<form class="contactform" action="mailto:lair.antoine89@orange.fr" method="POST">
+
+			<div class="row">
+				<div class="colleft">
+					<label class="form-label">Nom <span>*</span></label>
+					<input class="form-control" type="text" name="nom" placeholder="DOE">
+					<br />
+					<label class="form-label">Adresse mail <span>*</span></label>
+					<input class="form-control" type="email" name="email" placeholder="john.doe@domaine.fr">
+				</div>
+				<div class="colright">
+					<label class="form-label">Prénom <span>*</span></label>
+					<input class="form-control" type="text" name="prenom" placeholder="John">
+					<br />
+					<label class="form-label">Téléphone</label>
+					<input class="form-control" type="tel" name="tel" placeholder="0623456789">
+				</div>
+			</div>
+			<br />
+			<div class="row">
+				<label class="form-label message-label">Message <span>*</span></label>
+				<textarea class="form-control" name="message" placeholder="Taper votre message ici..."></textarea>
+			</div>
+			<input class="btn btn-secondary" type="submit" value="Envoyer">
+
+
+		</form>
+
+	</section>
 	<footer>
 		<p class="copyright">&copy; Antoine Lair - 2022</p>
 		<p>
